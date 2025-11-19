23ceacda8508a66b6429d5aea53563b052188c6c
Samikshya <samikshyabhusal066.com>
2025-11-19 13:59:17 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log("samikshya");
+//variable in javascript
+let program="HELLO";
+console.log(program);
+
+//constant in java script
+const pi=3.14;
+console.log(pi);
+
+let x=10;
+if(true){
+    let x=20;
+    console.log("Inside:",x);
+}
