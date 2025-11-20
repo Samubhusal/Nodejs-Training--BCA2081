e469bc30269de9db4e8c133cffeee1498ba88775
Samikshya <samikshyabhusal066.com>
2025-11-20 13:16:29 +0545

    odd or even

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// console.log("samikshya");
+// // console.log("samikshya");
 //variable in javascript
 let program="HELLO";
 console.log(program);
@@ -12,3 +12,33 @@ if(true){
     let x=20;
     console.log("Inside:",x);
 }
+console.log("output:",x);
+
+
+//Function in javascript
+//a function is a reusable block of code that perform a specific task
+//It helps avoid repetiton and make code organized 
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(5,3));
+
+function double(num){
+    return num*2;
+}
+console.log(double(5));
+
+function isEven(number){
+    if (number %2==0){
+        return `${number} is even`;
+    }else{
+        return`${number} is odd`;
+    }
+}
+
+function isEven(num){
+    return num % 2===0;
+}
+console.log(isEven(6));
+console.log(isOdd(7));
