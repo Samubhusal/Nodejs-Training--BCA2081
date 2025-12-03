ffe9fb73ae7cc5a13dc95dafe38f3cf705cd2e27
Samikshya <samikshyabhusal066.com>
2025-12-03 13:56:31 +0545

    post route

--- ExpressJS/form.js
+++ ExpressJS/form.js
@@ -1,18 +1,30 @@
-const express=require("express");
-const app=express();
-const port=5000;
+// const express=require("express");
+// const app=express();
+// const port=5000;
 
-//to read from data
-app.use(express.urlencoded({extended:true}));
 
-//POST route
-app.post("/contact",(req,res)=>{
-    const{name,email}=req.body;
-    res.send(`Received:Name=${name},Email=${email}`);
-});
+// // To read JSON body (Postman raw -> JSON)
+// app.use(express.json());
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server running 
-    at http://localhost:${port}`);
-});
+
+// //to read from data
+// app.use(express.urlencoded({extended:true}));
+
+// //POST route
+// app.post("/contact",(req,res)=>{
+//     const{name,email}=req.body;
+//    // res.send(`Received:Name=${name},Email=${email}`);
+//     res.send({
+//     message: "Data received successfully!",
+//     name: name,
+//     email: email
+//   });
+// });
+
+// // Start server
+// app.listen(port, () => {
+//   console.log(`Server running 
+//     at http://localhost:${port}`);
+// });
+
+
