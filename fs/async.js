202e16324b71b4e3e670e2e225106a553e3d7f7c
Samikshya <samikshyabhusal066.com>
2025-12-19 17:22:56 +0545

    async

---
+++ fs/async.js
@@ -0,0 +1,48 @@
+const fs=require("fs");
+const path=require("path");
+
+// const fileName="crud.txt";
+// const filePath=path.join(__dirname,fileName);
+// console.log(filePath);
+// fs.writeFile(filePath,"hello","utf-8",
+//     (err)=>{
+//         if(err) console.error(err);
+//         else console.log("File has been saved");
+//     }
+// )
+
+
+// const fileName="crud.txt";
+// const filePath=path.join(__dirname,fileName);
+// console.log(filePath);
+// fs.readFile(filePath,"utf-8",
+//     (err,data)=>{
+//         if(err) console.error(err);
+//         else console.log(data);
+//     }
+// )
+
+
+
+// const fileName="crud.txt";
+// const filePath=path.join(__dirname,fileName);
+// console.log(filePath);
+// fs.appendFile(filePath,"nice broo","utf-8",
+//     (err)=>{
+//         if(err) console.error(err);
+//         else console.log("saved the file");
+//     }
+// )
+
+
+
+
+const fileName="crud.txt";
+const filePath=path.join(__dirname,fileName);
+console.log(filePath);
+fs.unlink(filePath,
+    (err)=>{
+        if(err) console.error(err);
+        else console.log("delete file");
+    }
+);
---
+++ fs/create.js
@@ -0,0 +1,27 @@
+// writeFileSync(filePath,Data,optional)
+// readfileSync(filePath,optional)
+// appendFileSync(filePath,data,optional)
+// unlinkFileSync(filePath,optional)
+
+const fs=require("fs");
+const path=require("path");
+
+const fileName="crud.txt";
+const filePath=path.join(__dirname,fileName);
+console.log(filePath);
+
+// const writeFile=fs.writeFileSync(fileName,"this a create operation","utf-8");
+// console.log(writeFile);
+
+
+//const readFile=fs.readFileSync(filePath);
+//console.log(readFile.toString());
+
+// const updateFile=fs.appendFileSync(filePath,"don ho vai don","utf-8");
+// console.log(updateFile);
+
+ const deleteFile=fs.unlinkSync(filePath);
+ console.log(deleteFile);
+
+const readFile=fs.readFileSync(filePath,"utf-8");
+console.log(readFile);
